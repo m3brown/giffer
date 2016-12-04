61111b046d9ac76cf678ef0778e70d19880ccdfb
Mike Brown <mike.brown@excella.com>
2016-12-04 16:00:51 -0500

    remove hardcoded api key

--- giphy.py
+++ giphy.py
@@ -1,13 +1,16 @@
 import urllib2
 import json
+import os
+
+API_KEY = os.environ['GIPHY_API_KEY'] 
 
 def translate(search_text):
-    response = urllib2.urlopen('http://api.giphy.com/v1/gifs/translate?s=%s&api_key=dc6zaTOxFJmzC' % search_text.replace(' ', '+'))
+    response = urllib2.urlopen('http://api.giphy.com/v1/gifs/translate?s=%s&api_key=%s' % (search_text.replace(' ', '+'), API_KEY))
     data = json.load(response)['data']
     return data['images']['original']['url']
 
 def search(search_text):
-    response = urllib2.urlopen('http://api.giphy.com/v1/gifs/search?q=%s&limit=1&api_key=dc6zaTOxFJmzC' % search_text.replace(' ', '+'))
+    response = urllib2.urlopen('http://api.giphy.com/v1/gifs/search?q=%s&limit=1&api_key=%s' % (search_text.replace(' ', '+'), API_KEY))
     data = json.load(response)['data']
     if len(data) > 0:
         return data[0]['images']['original']['url']
